ca7955bb60f21a80445484d5e9f161ae6ea80dfd
Кочановский Денис Владимирович <kochanovskiidv@evolenta.ru>
2025-03-18 11:45:31 +0300

    Инверсия контроля. Внедрение зависимостей

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    Operation operation;
+
+    public Calculator (Operation operation) {
+        this.operation = operation;
+    }
+
+    public double calc(double a, double b) {
+        return operation.getResult(a, b);
+    }
+}
---
+++ src/Operation.java
@@ -0,0 +1,4 @@
+public interface Operation {
+
+    double getResult (double a, double b);
+}
